f42c3cfd0e37787670c9691b31f34314a6f007fe
wail <wail12@gmil.com>
2021-03-16 17:42:21 +0100

    react-js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import img2 from "./img.jpg"; //form src
+
+import React, { Fragment } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Fragment>
+            <div className="bag">
+                <nav className="navbar">
+                    <h1 className="titel">Mohamed Ali Touil</h1>
+                </nav>
+                <div
+                    style={{ border: "solid 1 black", width: "100%" }}
+                    className="items"
+                ></div>
+                <br />
+                <img src={img2} alt="img2" className="imgClass" />
+                <img src="Paris.jpg" alt="img3" className="dali" />
+                <br />
+                <video width="550" height="485" controls className="vid">
+                    <source src="myVideo.mp4" type="video/mp4" />
+                </video>
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
